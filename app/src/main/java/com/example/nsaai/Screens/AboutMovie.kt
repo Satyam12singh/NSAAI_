f8838548f995069785e1ec489846c60f1b1ff0c6
Satyam <satyamsingh20942@gmail.com>
2025-01-21 21:53:42 +0530

    HorizontalPager Setup with the bottom indication

--- app/src/main/java/com/example/nsaai/Screens/AboutMovie.kt
+++ app/src/main/java/com/example/nsaai/Screens/AboutMovie.kt
@@ -1,17 +1,27 @@
 package com.example.nsaai.Screens
 
 import android.util.Log
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.ui.util.lerp
 import androidx.compose.foundation.pager.HorizontalPager
@@ -22,9 +32,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -59,72 +74,110 @@ fun AboutMovie(modifier: Modifier = Modifier,
 
     viewmodel.fetchAboutTheMovie(externalid_fetched)
     val poster_path = viewmodel.posterPath.value
-    val image_path =viewmodel.imageofmovie.value
+    val image_path = viewmodel.imageofmovie.value
     Log.d("AM poster path", "$poster_path")
 //    val scrollState = rememberScrollState()
 
     val pagerState = rememberPagerState(pageCount = { 2 }) // Two pages
 
-    HorizontalPager(state = pagerState) { page ->
-        when (page) {
-            0 -> {
-                    Column(
-                        modifier= Modifier.fillMaxSize()
-                            .background(brush = Brush.verticalGradient(
-                                colors = listOf(Color.Black, MaterialTheme.colors.background),
-                                endY = 5000f
-                            ))
+    Box(modifier = modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colors.background)
+    ){
+        HorizontalPager(state = pagerState) { page ->
 
-                        ,
+            when (page) {
+                0 -> {
+
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                            .background(
+                                brush = Brush.verticalGradient(
+                                    colors = listOf(Color.Black, MaterialTheme.colors.background),
+                                    endY = 5000f
+                                )
+                            ),
 
                         horizontalAlignment = Alignment.CenterHorizontally,
                         verticalArrangement = Arrangement.Top
 
                     ) {
+
+
+
                         Box(
-                            modifier= Modifier
+                            modifier = Modifier
                                 .fillMaxSize()
                                 .wrapContentSize()
-                                .padding(vertical = 10.dp, horizontal =30.dp)
+                                .padding(vertical = 10.dp, horizontal = 30.dp)
                                 .clip(RoundedCornerShape(10.dp)),
                             contentAlignment = Alignment.TopCenter
-                        ){
-                            Image(painter = rememberAsyncImagePainter(model = "https://image.tmdb.org/t/p/original/${poster_path}"),
-                                contentDescription = null)
+                        ) {
+                            Image(
+                                painter = rememberAsyncImagePainter(model = "https://image.tmdb.org/t/p/original/${poster_path}"),
+                                contentDescription = null
+                            )
 
                             Card(
-                                modifier=Modifier
+                                modifier = Modifier
                                     .align(Alignment.BottomCenter)
                                     .size(40.dp)
-                                    .border(width = 2.dp, color = Color.White, shape = CircleShape)
+                                    .border(width = 2.dp, color = Color.Gray, shape = CircleShape)
                                     .clip(CircleShape)
-                                    .padding(2.dp)
-                                ,
+                                    .padding(2.dp),
                                 elevation = 20.dp,
                                 backgroundColor = Color.White
 
 
                             )
-                             {
-                                Image(painter = rememberAsyncImagePainter(model = "https://image.tmdb.org/t/p/w500${image_path}"),
+                            {
+                                Image(
+                                    painter = rememberAsyncImagePainter(model = "https://image.tmdb.org/t/p/w500${image_path}"),
                                     contentDescription = null,
-                                    modifier= Modifier.fillMaxSize())
-                             }
+                                    modifier = Modifier.fillMaxSize()
+                                )
+                            }
                         }
+
                     }
 
 
-            }
-            1->{
-                Column(
-                    modifier=Modifier.fillMaxSize()
-                ) {
-                    Text("Hello")
+                }
+
+                1 -> {
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Text("Hello")
+                    }
                 }
             }
         }
+        Row(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(pagerState.pageCount) { iteration ->
+                val color =
+                    if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                Box(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(12.dp)
+                )
+            }
+        }
     }
-}
+
+
+    }
+
 
 //Image(
 //painter = rememberAsyncImagePainter(model ="https://image.tmdb.org/t/p/w500${image_path}"),
@@ -145,3 +198,4 @@ fun AboutMovie(modifier: Modifier = Modifier,
 //private fun AboutMoviePreview() {
 //    AboutMovie(viewmodel= AboutMovieViewModel(), viewModel = MovieViewModel(), id = 762509)
 //}
+
